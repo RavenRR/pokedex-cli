cec839bb3333325e76c5a7805bcea2052de0a352
RavenRR <romerorraven@icloud.com>
2021-12-01 13:45:18 -0700

    Add menu method

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,10 +1,15 @@
 class CLI
     def run
         greeting
+        menu
     end
 
     def greeting
         puts "Welcome Pokemaster!"
+    end
+
+    def menu
+        puts "Select a Pokemon"
     end
 
 end
