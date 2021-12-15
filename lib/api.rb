d72734d9d9245132edf1b1e4fae4f0bcd8195895
RavenRR <romerorraven@icloud.com>
2021-12-15 01:48:09 -0700

    hash results/iterate over pokemons array

--- lib/api.rb
+++ lib/api.rb
@@ -1,8 +1,14 @@
 class API
     def self.grab_pokemons
+
         url = "https://pokeapi.co/api/v2/pokemon?limit=151"
         uri = URI(url)
         response = Net::HTTP.get(uri)
         hash = JSON.parse(response)
+        pokemons_array = hash["results"]
+
+        pokemons_array.each do |pokemon|
+            Pokemon.new(name: pokemon["name"], url: pokemon["url"])
+        end
     end
 end
