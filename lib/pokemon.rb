ecb9d3e30d0134a605cc4f623ece6b9994faa59d
RavenRR <romerorraven@icloud.com>
2021-12-14 22:32:04 -0700

    Add attr_accessor/ @@all class variable

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,6 +1,16 @@
 class Pokemon
+
+    attr_accessor :name, :url, :height, :weight, :type, :id
+
+    @@all = [ ]
+
     def initialize(name:, url:)
         @name = name
         @url = url
+        @@all << self
+    end
+
+    def self.all
+        @@all
     end
 end
