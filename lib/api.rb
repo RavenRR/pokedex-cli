50efde9c6f30e4082b98f7c19cfc2b21370437c7
RavenRR <romerorraven@icloud.com>
2021-12-14 20:15:00 -0700

    Add instance method/Add api url/parse respon json

--- lib/api.rb
+++ lib/api.rb
@@ -1,2 +1,8 @@
 class API
+    def self.grab_pokemons
+        url = "https://pokeapi.co/api/v2/pokemon?limit=151"
+        uri = URI(url)
+        response = Net::HTTP.get(uri)
+        hash = JSON.parse(response)
+    end
 end
