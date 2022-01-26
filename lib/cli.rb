b4d87ca6701c00ece238f7335e0084bc9c10ba83
RavenRR <romerorraven@icloud.com>
2022-01-25 20:23:16 -0700

    Add user choice method/display pokemon info

--- lib/cli.rb
+++ lib/cli.rb
@@ -18,6 +18,7 @@ class CLI
         if input == "y"
             puts "Here we go!"
             self.display_list_of_pokemons
+            self.user_pokemon_choice
         elsif input == "n"
             puts "Farewell!"
         else
@@ -32,5 +33,45 @@ class CLI
             puts "#{index+1}. #{pokemon.name}" 
         
         end
+    end
+    def user_pokemon_choice
+
+        puts"----------------------------"
+        puts "Enter a number to the Pokemon you'd like to learn more about."
+        puts"----------------------------"
+        pokemon_choice_index = gets.strip.to_i - 1
+        max_limit = Pokemon.all.length - 1
+        puts"----------------------------"
+
+        until pokemon_choice_index.between?(0,max_limit)
+            puts "Invalid choice"
+            pokemon_choice_index = gets.strip.to_i - 1  #If invalid user will be asked again
+        end
+
+        #User's pokemon choice
+        pokemon_object_lookup = Pokemon.all[pokemon_choice_index]
+        sleep(2)
+        puts "Let's see what there is to learn!"
+        puts"----------------------------"
+        sleep(2)
+        API.pokemon_info(pokemon_object_lookup)
+        self.display_pokemon_info(pokemon_object_lookup)
+    end
+
+    def display_pokemon_info(pokemon_object_lookup)
+        #details displayed for user
+        
+        puts "Name: #{pokemon_object_lookup.name}"
+        sleep(1)
+        puts "Type: #{pokemon_object_lookup.type}"
+        sleep(1)
+        puts "Ability: #{pokemon_object_lookup.abilities}"
+        sleep(1)
+        puts "Move: #{pokemon_object_lookup.moves}"
+        sleep(1)
+        puts "Height: #{pokemon_object_lookup.height}"
+        sleep(1)
+        puts "Weight: #{pokemon_object_lookup.weight}"
+        sleep(1)
     end
 end
